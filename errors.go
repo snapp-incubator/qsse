2d34bc8958fc406e918b4df3c6bd9f1274bbbed1
Mehdi <mehditeymorian322@gmail.com>
2022-05-11 08:50:32 +0430

    feat: add common errors

---
+++ errors.go
@@ -0,0 +1,30 @@
+package qsse
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+type Error struct {
+	Err  error `json:"err,omitempty"`
+	Code int   `json:"code,omitempty"`
+}
+
+const ErrorTopic = "error"
+
+var ErrNotAuthorized = errors.New("not authorized")
+
+const (
+	CodeNotAuthorized = iota + 1
+	CodeTopicNotAvailable
+)
+
+func ErrTopicNotAvailable(topic string) Error {
+	return Error{Err: errors.New("topic not available: " + topic), Code: CodeTopicNotAvailable}
+}
+
+func UnmarshalError(bytes []byte) Error {
+	var err Error
+	json.Unmarshal(bytes, &err)
+	return err
+}
