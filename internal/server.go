f3fd75f433509c46145542c51daade83c179373c
ahmad.mohammadi <ahmad.mohammadi@snapp.cab>
2022-05-24 15:25:07 +0430

    improve publish function to publish in wildCard topic

--- internal/server.go
+++ internal/server.go
@@ -16,6 +16,7 @@ type Server struct {
 	Worker       Worker
 	Listener     quic.Listener
 	EventSources map[string]*EventSource
+	Topics       []string
 
 	Authenticate func(token string) bool
 }
@@ -27,8 +28,17 @@ var DefaultAuthenticationFunc = func(token string) bool { //nolint:gochecknoglob
 
 // Publish publishes an event to all the subscribers of the given topic.
 func (s *Server) Publish(topic string, event []byte) {
-	if source, ok := s.EventSources[topic]; ok {
-		source.DataChannel <- event
+	if !topicHasWildcard(topic) {
+		if source, ok := s.EventSources[topic]; ok {
+			source.DataChannel <- event
+		}
+	} else {
+		matchedTopics := findTopicsList(s.Topics, topic)
+		for _, matchedTopic := range matchedTopics {
+			if source, ok := s.EventSources[matchedTopic]; ok {
+				source.DataChannel <- event
+			}
+		}
 	}
 }
 
