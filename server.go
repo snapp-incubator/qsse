5a56fd759ce0963fe6bf66284b26fd9b3a10bc36
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-06-08 09:47:17 +0430

    feat: allow nil config

--- server.go
+++ server.go
@@ -10,7 +10,7 @@ import (
 )
 
 type ServerConfig struct {
-	Metric MetricConfig
+	Metric *MetricConfig
 }
 
 type MetricConfig struct {
@@ -31,6 +31,8 @@ type Server interface {
 
 // NewServer creates a new server and listen for connections on the given address.
 func NewServer(address string, tlsConfig *tls.Config, topics []string, config *ServerConfig) (Server, error) {
+	config = processServerConfig(config)
+
 	listener, err := quic.ListenAddr(address, tlsConfig, nil)
 	if err != nil {
 		return nil, errors.Errorf("failed to listen at address %s: %s", address, err.Error())
@@ -52,4 +54,26 @@ func NewServer(address string, tlsConfig *tls.Config, topics []string, config *S
 	go server.AcceptClients()
 
 	return &server, nil
+}
+
+func processServerConfig(cfg *ServerConfig) *ServerConfig {
+	if cfg == nil {
+		return &ServerConfig{
+			Metric: &MetricConfig{
+				Enabled:   false,
+				NameSpace: "qsse",
+				Port:      "8081",
+			},
+		}
+	}
+
+	if cfg.Metric == nil {
+		cfg.Metric = &MetricConfig{
+			Enabled:   false,
+			NameSpace: "qsse",
+			Port:      "8081",
+		}
+	}
+
+	return cfg
 }
